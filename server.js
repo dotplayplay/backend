2c2957f854cc293c1ff0ed9c744d671e21da54a1
highscoretech <highscoretechng@gmail.com>
2023-11-05 05:35:49 +0100

    first commit

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const Bonus = require('./routes/bonus')
 require("dotenv").config();
 // ============ Initilize the app ========================
 
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
