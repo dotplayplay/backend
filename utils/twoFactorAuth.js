66f859a0633b3544394b06402179b1cff45e3050
unyimeudoh <unyimeudemy20@gmail.com>
2023-12-10 22:19:52 +0100

    2fa initiated

--- utils/twoFactorAuth.js
+++ utils/twoFactorAuth.js
@@ -1,7 +1,7 @@
 const speakeasy = require("speakeasy");
 const qrcode = require("qrcode");
 
-const secret = speakes
+// const secret = speakes
 
 const twoFactorAuth = async (req, res) => {
     res.json({message: "welcome to two factor auth"});
