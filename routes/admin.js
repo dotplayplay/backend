89f4c8c01ba89f2a17a6f00bb720d5d1db2bba05
Abdul Azeez Opeyemi Adegoke <adegokeabdulazeez653@gmail.com>
2023-12-11 19:44:03 +0100

    last week final update with this week task

--- routes/admin.js
+++ routes/admin.js
@@ -1,26 +1,49 @@
 
 const express = require('express')
-const requireAuth = require('../middleware/requireAuth')
 const {
-    createMember, 
-    getAllMembers, 
-    adminDashbaord, 
-    findUserById, 
-    findUserByUsername, 
-    registeredUserstats, 
-    totalWageredAndTotalWon, 
-    totalWageredRanking, 
-    totalWonRanking, 
+    createMember,
+    getAllMembers,
+    adminDashbaord,
+    findUserById,
+    findUserByUsername,
+    registeredUserstats,
+    totalWageredAndTotalWon,
+    totalWageredRanking,
+    totalWonRanking,
     totalLossRanking,
     dailyReport,
     gameReport,
     ggrReport
 } = require('../controller/adminController')
+const { login, register, findAdminById, findAdminByUsername, updatePin, updatePassword, suspend, role, updateAvailability,removeAdmin, getAllAdmin, createChatSettings, updateChatSettings, getChatSettings } = require('../controller/adminAuthController')
+const { protect } = require('../middleware/auth')
 
 const router = express.Router()
 
+/* ADMIN AUTH */
+//POST
+router.post('/auth/login', login)
+router.post('/auth/register', register)
 
-/* READ */
+//PATCH
+router.patch('/pin', protect, updatePin)
+router.patch('/password', protect, updatePassword)
+router.patch('/suspend', protect, suspend)
+router.patch('/role', protect, role)
+router.patch('/avalability', protect, updateAvailability)
+
+//DELETE
+router.delete('/remove/:id', protect, removeAdmin)
+
+//GET
+router.get('/listadmins', protect, getAllAdmin)
+router.get('/:id', protect, findAdminById)
+router.get('/username/:username', protect, findAdminByUsername)
+
+
+
+
+/* READ DASHBOARD AND REPORT */
 
 /* Get Admin Dashboard */
 router.get('/dashboard', adminDashbaord)
@@ -42,6 +65,11 @@ router.get('/ggrreport', ggrReport)
 
 /* Create Member */
 router.post('/create', createMember)
+
+//Chat Settings
+router.post('/chatsettings', protect, createChatSettings)
+router.patch('/chatsettings', protect, updateChatSettings)
+router.get('/chatsettings/get', protect, getChatSettings)
 
 
 module.exports = router
