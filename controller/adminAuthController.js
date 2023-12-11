e51358698a23302138c3d7d9cd09dc3dd95c29c8
Abdul Azeez Opeyemi Adegoke <adegokeabdulazeez653@gmail.com>
2023-12-11 21:02:44 +0100

    last week final update with this week task

--- controller/adminAuthController.js
+++ controller/adminAuthController.js
@@ -93,42 +93,42 @@ const register = async (req, res, next) => {
 
 // LOGIN
 const login = async (req, res, next) => {
-    try{
-    const { username, password } = req.body;
-    if (!username || !password) {
-        return res.status(400).json({
-            success: false,
-            message: "Kindly provide a Username and password"
-        });
-    }
-    const user = await Admin.findOne({ username }).select("+password");
-    if (!user) {
-        return res.status(401).json({
-            success: false,
-            message: "Invalid Credentials"
-        });
-    }
-    if (!(await user.verifyPassword(password))) {
-        return res.status(401).json({
-            success: false,
-            message: "Invalid Credentials"
-        });
-    }
+    try {
+        const { username, password } = req.body;
+        if (!username || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Kindly provide a Username and password"
+            });
+        }
+        const user = await Admin.findOne({ username }).select("+password");
+        if (!user) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Credentials"
+            });
+        }
+        if (!(await user.verifyPassword(password))) {
+            return res.status(401).json({
+                success: false,
+                message: "Invalid Credentials"
+            });
+        }
 
-    //Log Activity
-    await createActivityLog(user._id, "Logged In", req)
+        //Log Activity
+        await createActivityLog(user._id, "Logged In", req)
 
-    await Admin.findOneAndUpdate({ _id: user._id }, { lastLogin: Date.now() });
+        await Admin.findOneAndUpdate({ _id: user._id }, { lastLogin: Date.now() });
 
-    sendTokenResponse(user, 200, res);
-}catch(err){
-    return res.json({ error: err})
-}
+        sendTokenResponse(user, 200, res);
+    } catch (err) {
+        return res.json({ error: err })
+    }
 };
 //FIND BY ID
 const findAdminById = async (req, res, next) => {
     try {
-    const { id } = req.params
+        const { id } = req.params
         const user = await Admin.findById(id)
         if (!user) {
             return res.status(401).json({
@@ -136,7 +136,7 @@ const findAdminById = async (req, res, next) => {
                 message: "No user found with this ID"
             });
         }
-        const userActivityLog = await AdminActivityLog.find({ admin_id: id }).sort({createdAt: -1})
+        const userActivityLog = await AdminActivityLog.find({ admin_id: id }).sort({ createdAt: -1 })
         return res.status(401).json({
             success: true,
             data: user,
@@ -157,22 +157,22 @@ const findAdminByUsername = async (req, res, next) => {
                 message: "No user found with this ID"
             });
         }
-        const userActivityLog = await AdminActivityLog.find({ admin_id: req.user.id }).sort({createdAt: -1})
+        const userActivityLog = await AdminActivityLog.find({ admin_id: req.user.id }).sort({ createdAt: -1 })
         return res.status(401).json({
             success: true,
             data: user,
             activity: userActivityLog || null
         });
     } catch (err) {
-        // return res.json({ error: err })
-        console.log(err)    
+        return res.json({ error: err })
+        // console.log(err)    
     }
 }
 //UPDATE PIN
 const updatePin = async (req, res, next) => {
-    const id = req.user.id
-    const { pin } = req.body
     try {
+        const id = req.user.id
+        const { pin } = req.body
         const user = await Admin.findById(id)
         if (!user) {
             return res.status(401).json({
@@ -196,9 +196,9 @@ const updatePin = async (req, res, next) => {
 }
 //UPDATE PASSWORD
 const updatePassword = async (req, res, next) => {
-    const id = req.user.id
-    const { password } = req.body
     try {
+        const id = req.user.id
+        const { password } = req.body
         const user = await Admin.findById(id)
         if (!user) {
             return res.status(401).json({
@@ -222,14 +222,14 @@ const updatePassword = async (req, res, next) => {
 }
 //UPDATE SUSPEND
 const suspend = async (req, res, next) => {
-    const { user_id } = req.body
-    if (!(req.user.role === 'superadmin')) {
-        return res.status(401).json({
-            success: false,
-            message: "Only Staff with Super Admin Priviledge can perform this operation"
-        });
-    }
     try {
+        const { user_id } = req.body
+        if (!(req.user.role === 'superadmin')) {
+            return res.status(401).json({
+                success: false,
+                message: "Only Staff with Super Admin Priviledge can perform this operation"
+            });
+        }
         const user = await Admin.findById(user_id)
         if (!user) {
             return res.status(401).json({
@@ -253,14 +253,14 @@ const suspend = async (req, res, next) => {
 }
 //UPDATE ROLE
 const role = async (req, res, next) => {
-    const { user_id, role } = req.body
-    if (!(req.user.role === 'superadmin')) {
-        return res.status(401).json({
-            success: false,
-            message: "Only Staff with Super Admin Priviledge can perform this operation"
-        });
-    }
     try {
+        const { user_id, role } = req.body
+        if (!(req.user.role === 'superadmin')) {
+            return res.status(401).json({
+                success: false,
+                message: "Only Staff with Super Admin Priviledge can perform this operation"
+            });
+        }
         const user = await Admin.findById(user_id)
         if (!user) {
             return res.status(401).json({
@@ -284,8 +284,8 @@ const role = async (req, res, next) => {
 }
 //UPDATE AVAILABILITY
 const updateAvailability = async (req, res, next) => {
-    const id = req.user.id
     try {
+        const id = req.user.id
         const user = await Admin.findById(id)
         if (!user) {
             return res.status(401).json({
@@ -314,14 +314,14 @@ const updateAvailability = async (req, res, next) => {
 }
 //REMOVE ADMIN
 const removeAdmin = async (req, res, next) => {
-    const { id } = req.params
-    if (!(req.user.role === 'superadmin')) {
-        return res.status(401).json({
-            success: false,
-            message: "Only Staff with Super Admin Priviledge can perform this operation"
-        });
-    }
     try {
+        const { id } = req.params
+        if (!(req.user.role === 'superadmin')) {
+            return res.status(401).json({
+                success: false,
+                message: "Only Staff with Super Admin Priviledge can perform this operation"
+            });
+        }
         const user = await Admin.deleteOne({ _id: id })
         if (user.deletedCount <= 0) {
             return res.status(401).json({
@@ -380,8 +380,8 @@ const createChatSettings = async (req, res, next) => {
 }
 //Update chat Settings
 const updateChatSettings = async (req, res, next) => {
-    const data = req.body
     try {
+        const data = req.body
         const result = await AdminChatSettings.updateOne({ _id: data._id }, {
             $set: {
                 "rain_bot": {
