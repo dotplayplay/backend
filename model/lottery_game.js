432a903248f152d40a97a7025753834c00109076
softsmartsolutions <chief.developer@softsmartsolutions.com>
2023-12-12 02:36:39 +0100

    Fixed lottery number generation

--- model/lottery_game.js
+++ model/lottery_game.js
@@ -30,7 +30,7 @@ const LotterySchema = new schema({
         default: function() {
             //return Date.now() + 60000;
             let now = new Date();
-            let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate()+1, 15, 0, 0, 0);
+            let tomorrow = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 15, 0, 0, 0);
             if (now.getUTCHours() >= 15) {
                 tomorrow.setDate(tomorrow.getDate() + 1);
             }
