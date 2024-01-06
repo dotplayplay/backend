c5895ea04f9574364045aa6532c2ce958b5ecdfc
Elvis O <elvis.osujic@gmail.com>
2024-01-06 12:52:43 +0100

    keno-game endpoints

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const AllPLays = require("./routes/admin/PLayers/crashPlayers");
 const Transaction = require("./routes/transactions.js");
 const Stats = require("./routes/admin/statistic/statistics");
@@ -12,26 +12,27 @@ const Admin = require("./routes/admin.js");
 const Promotion = require("./routes/promotion.js");
 const Profile = require("./routes/Profile.js");
 const Chat = require("./routes/chat");
-const Notify = require("./routes/notify.js")
+const Notify = require("./routes/notify.js");
 // require("./controller/lotteryEngine.js");
 // require("./controller/cronScheduler.js");
-require("./controller/rainCronScheduller.js")
+require("./controller/rainCronScheduller.js");
 const minegame = require("./routes/mines");
 const Wallet = require("./routes/wallet.js");
 const diceGame = require("./routes/diceGame");
+const kenoGame = require("./routes/kenoGame");
 const Deposit = require("./routes/deposit");
-const Withdraw = require("./routes/withdraw")
-const Bonus = require('./routes/bonus');
-const Lottery = require('./routes/lottery');
-const HiloGame = require('./routes/hiloGame.js');
+const Withdraw = require("./routes/withdraw");
+const Bonus = require("./routes/bonus");
+const Lottery = require("./routes/lottery");
+const HiloGame = require("./routes/hiloGame.js");
 const TransactionHistory = require("./routes/transactionHistory.js");
 const homePageUpdates = require("./routes/homePageUpdates");
 const { createsocket } = require("./socket/index.js");
 const { createServer } = require("node:http");
 //Test
 
-const {testCashback} = require("./profile_mangement/week_cashback")
-const {testResetCashback} = require("./profile_mangement/rechargebonus")
+const { testCashback } = require("./profile_mangement/week_cashback");
+const { testResetCashback } = require("./profile_mangement/rechargebonus");
 
 require("dotenv").config();
 // ============ Initilize the app ========================
@@ -49,6 +50,7 @@ main();
 // application routes
 app.use("/api/user/crash-game", CrashGame);
 app.use("/api/user/dice-game", diceGame);
+app.use("/api/user/keno-game", kenoGame);
 app.use("/api/user/mine-game", minegame);
 app.use("/api/hilo-game", HiloGame);
 app.use("/api/users", User);
@@ -66,30 +68,30 @@ app.use("/api/lottery", Lottery);
 app.use("/api/transaction-history", TransactionHistory);
 app.use("/admin/all-players", AllPLays);
 app.use("/api/home-page-updates", homePageUpdates);
-app.use('/api/promotion', Promotion);
+app.use("/api/promotion", Promotion);
 //TODO: REMOVE
 app.post("/api/test-cashback", testCashback);
 //TODO: REMOVE
-app.post("/api/deactivate-recharge", testResetCashback)
-
+app.post("/api/deactivate-recharge", testResetCashback);
 
 //admin routes
-app.use('/admin', Admin);
+app.use("/admin", Admin);
 //Notification
-app.use('/api/notifies', Notify);
+app.use("/api/notifies", Notify);
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   res.send("Welcome to Dotplayplay backend server");
-})
+});
 
-mongoose.set('strictQuery', false);
+mongoose.set("strictQuery", false);
 // const dbUri = `mongodb+srv://highscoreteh:eNiIQbm4ZMSor8VL@cluster0.xmpkpjc.mongodb.net/main_page?retryWrites=true&w=majority`
 
 const dbUri = `mongodb://localhost:27017/dpp`;
-mongoose.connect(dbUri, { useNewUrlParser: true,  useUnifiedTopology: true })
-    .then((result)=>  console.log('Database connected'))
-    .catch((err)=> console.log(err))
+mongoose
+  .connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => console.log("Database connected"))
+  .catch((err) => console.log(err));
 const PORT = process.env.PORT || 8000;
-server.listen(PORT, ()=>{
-    console.log("Running on port "+ PORT)
-})
+server.listen(PORT, () => {
+  console.log("Running on port " + PORT);
+});
