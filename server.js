1be80aa6464488270b93239237cbc70de4a686b6
Elvis O <elvis.osujic@gmail.com>
2024-01-17 07:19:03 +0100

    conflicts

--- server.js
+++ server.js
@@ -91,8 +91,7 @@ mongoose.set("strictQuery", false);
 const dbUri = `mongodb+srv://highscoreteh:eNiIQbm4ZMSor8VL@cluster0.xmpkpjc.mongodb.net/main_page?retryWrites=true&w=majority`;
 
 // const dbUri = `mongodb://localhost:27017/dpp`;
-mongoose
-  .connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => console.log("Database connected"))
   .catch((err) => console.log(err));
 const PORT = process.env.PORT || 8000;
